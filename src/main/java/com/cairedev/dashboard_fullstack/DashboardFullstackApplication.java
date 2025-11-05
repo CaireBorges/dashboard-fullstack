eab5ca230780f1b771d037db3ca9d9475b74e72d
CaireBorges <caireborges99@gmail.com>
2025-11-05 11:02:31 -0300

    Project created

---
+++ src/main/java/com/cairedev/dashboard_fullstack/DashboardFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.cairedev.dashboard_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cairedev/dashboard_fullstack/DashboardFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cairedev.dashboard_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
